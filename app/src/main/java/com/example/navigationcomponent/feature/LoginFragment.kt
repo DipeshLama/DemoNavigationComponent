dfc772676bced03a9ae231bafd8dbad175961744
DipeshLama <dipeslama32@gmail.com>
2022-05-02 12:25:03 +0545

    Simple demo using Nav component

---
+++ app/src/main/java/com/example/navigationcomponent/feature/LoginFragment.kt
@@ -0,0 +1,61 @@
+package com.example.navigationcomponent.feature
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.navigationcomponent.R
+import com.example.navigationcomponent.databinding.FragmentLoginBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class LoginFragment : Fragment(), View.OnClickListener {
+    private lateinit var binding : FragmentLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        activity?.findViewById<BottomNavigationView>(R.id.btmNav)?.visibility =View.GONE
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentLoginBinding.bind(view)
+        initListener()
+    }
+
+    private fun initListener (){
+        binding.btnLogin.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View?) {
+        when (view ){
+            binding.btnLogin -> {
+                navigateToHome()
+            }
+            binding.txvNavigateToSignUp ->{
+                navigateToSignUp()
+            }
+        }
+    }
+
+    private fun navigateToSignUp(){
+        findNavController().navigate(R.id.action_loginFragment_to_signUpFragment)
+    }
+
+    private fun navigateToHome (){
+        findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        activity?.findViewById<BottomNavigationView>(R.id.btmNav)?.visibility = View.VISIBLE
+    }
+}
---
+++ app/src/main/java/com/example/navigationcomponent/feature/SignUpFragment.kt
@@ -0,0 +1,55 @@
+package com.example.navigationcomponent.feature
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.navigationcomponent.R
+import com.example.navigationcomponent.databinding.FragmentSignUpBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class SignUpFragment : Fragment(), View.OnClickListener {
+    private lateinit var binding : FragmentSignUpBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        activity?.findViewById<BottomNavigationView>(R.id.btmNav)?.visibility =View.GONE
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentSignUpBinding.bind(view)
+        initListener()
+    }
+
+    private fun initListener(){
+        binding.txvNavigateToLogin.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View?) {
+        when(view) {
+            binding.txvNavigateToLogin ->{
+                navigateToLogin()
+            }
+        }
+    }
+
+    private fun navigateToLogin(){
+        findNavController().navigate(R.id.action_signUpFragment_to_loginFragment)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        activity?.findViewById<BottomNavigationView>(R.id.btmNav)?.visibility = View.VISIBLE
+    }
+}
